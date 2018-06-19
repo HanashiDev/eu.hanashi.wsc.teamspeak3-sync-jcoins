5716ad333b530c8e9a3751925e2b50f648130025
Peter Lohse <peterlohse@yahoo.de>
2018-06-19 15:27:10 +0200

    initial commit

---
+++ make.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+del files.tar
+7z a -ttar -mx=9 files.tar .\files\*
+del eu.hanashi.wsc.teamspeak3-sync-jcoins.tar
+7z a -ttar -mx=9 eu.hanashi.wsc.teamspeak3-sync-jcoins.tar .\* -x!files -x!eu.hanashi.wsc.teamspeak3-sync-jcoins.tar -x!.git -x!.gitignore -x!make.bat
