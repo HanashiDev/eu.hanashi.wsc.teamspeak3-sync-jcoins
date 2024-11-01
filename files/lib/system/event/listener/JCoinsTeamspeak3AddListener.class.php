b718b6d39dd54e0a709a5fc6be221c51b3a98ee4
Peter Lohse <hanashi@hanashi.eu>
2024-11-01 17:40:09 +0100

    update codestyle

--- files/lib/system/event/listener/JCoinsTeamspeak3AddListener.class.php
+++ files/lib/system/event/listener/JCoinsTeamspeak3AddListener.class.php
@@ -1,24 +1,34 @@
 <?php
+
 namespace wcf\system\event\listener;
+
 use wcf\system\user\jcoins\UserJCoinsStatementHandler;
 use wcf\system\WCF;
 
-class JCoinsTeamspeak3AddListener implements IParameterizedEventListener {
-	public function execute($eventObj, $className, $eventName, array &$parameters) {
-		if (!MODULE_JCOINS || !WCF::getUser()->userID) return;
-		
-		if ($eventObj->getActionName() == 'create') {
-			$paramas = $eventObj->getParameters();
-			
-			UserJCoinsStatementHandler::getInstance()->create('de.wcflabs.jcoins.statement.teamspeak3.start', null, [
-				'userID' => $paramas['data']['userID']
-			]);
-		} else if ($eventObj->getActionName() == 'delete') {
-			foreach ($eventObj->getObjects() as $object) {
-				UserJCoinsStatementHandler::getInstance()->revoke('de.wcflabs.jcoins.statement.teamspeak3.start', $object->getDecoratedObject(), [
-					'userID' => $object->userID
-				]);
-			}
-		}
-	}
+class JCoinsTeamspeak3AddListener implements IParameterizedEventListener
+{
+    public function execute($eventObj, $className, $eventName, array &$parameters)
+    {
+        if (!MODULE_JCOINS || !WCF::getUser()->userID) {
+            return;
+        }
+
+        if ($eventObj->getActionName() == 'create') {
+            $paramas = $eventObj->getParameters();
+
+            UserJCoinsStatementHandler::getInstance()->create('de.wcflabs.jcoins.statement.teamspeak3.start', null, [
+                'userID' => $paramas['data']['userID'],
+            ]);
+        } elseif ($eventObj->getActionName() == 'delete') {
+            foreach ($eventObj->getObjects() as $object) {
+                UserJCoinsStatementHandler::getInstance()->revoke(
+                    'de.wcflabs.jcoins.statement.teamspeak3.start',
+                    $object->getDecoratedObject(),
+                    [
+                        'userID' => $object->userID,
+                    ]
+                );
+            }
+        }
+    }
 }
