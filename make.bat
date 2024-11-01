b397c1f3edd78a5654551d789b4e65888134a69e
Peter Lohse <hanashi@hanashi.eu>
2024-11-01 17:38:35 +0100

    update workflow

--- make.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-del files.tar
-7z a -ttar -mx=9 files.tar .\files\*
-del eu.hanashi.wsc.teamspeak3-sync-jcoins.tar
-7z a -ttar -mx=9 eu.hanashi.wsc.teamspeak3-sync-jcoins.tar .\* -x!files -x!eu.hanashi.wsc.teamspeak3-sync-jcoins.tar -x!.git -x!.gitignore -x!make.bat -x!make.sh
--- make.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -f files.tar
-7z a -ttar -mx=9 files.tar ./files/*
-rm -f eu.hanashi.wsc.teamspeak3-sync-jcoins.tar
-7z a -ttar -mx=9 eu.hanashi.wsc.teamspeak3-sync-jcoins.tar ./* -x!files -x!eu.hanashi.wsc.teamspeak3-sync-jcoins.tar -x!.git -x!.gitignore -x!make.bat -x!make.sh
