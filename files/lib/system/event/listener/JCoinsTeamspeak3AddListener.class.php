36dcd1d73b2262f9521483e9502ccf263e4fcbb9
Peter Lohse <hanashi@hanashi.eu>
2024-11-01 17:45:33 +0100

    update for 6.0

--- files/lib/system/event/listener/JCoinsTeamspeak3AddListener.class.php
+++ files/lib/system/event/listener/JCoinsTeamspeak3AddListener.class.php
@@ -5,9 +5,9 @@ namespace wcf\system\event\listener;
 use wcf\system\user\jcoins\UserJCoinsStatementHandler;
 use wcf\system\WCF;
 
-class JCoinsTeamspeak3AddListener implements IParameterizedEventListener
+final class JCoinsTeamspeak3AddListener extends AbstractEventListener
 {
-    public function execute($eventObj, $className, $eventName, array &$parameters)
+    protected function onFinalizeAction($eventObj)
     {
         if (!MODULE_JCOINS || !WCF::getUser()->userID) {
             return;
